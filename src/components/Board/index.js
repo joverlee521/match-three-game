123b49ea9b5b456da4b713cd58946fa5469d58fe
J.Lee <joverlee521@gmail.com>
2019-01-09 16:18:34 -0800

    now tiles can be swapped! Only tiles right next to each other can swap

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -10,6 +10,7 @@ const containerStyle = {
 
 export default class GameBoard extends Component {
     state = {
+        gameStarted: false,
         tile: [
             ['', '', '', '', '', ''],
             ['', '', '', '', '', ''],
@@ -17,30 +18,86 @@ export default class GameBoard extends Component {
             ['', '', '', '', '', ''],
             ['', '', '', '', '', ''],
             ['', '', '', '', '', '']
-        ]
+        ],
+        firstClick: {},
+        secondClick: {}
     }
 
-    randomColor = () => {
+    randomTile = () => {
         const randomNumber = Math.floor(Math.random() * 3);
-        const colors = ["Red", "Green", "Blue"];
-        return colors[randomNumber];
+        const tiles = [
+            {shape: "square", color: "red"},
+            {shape: "triangle", color: "green"},
+            {shape: "circle", color: "blue"}
+        ];
+        return tiles[randomNumber];
+    }
+
+    startBoard = () => {
+        let newState = [];
+        this.state.tile.map((row, xIndex) => {
+            let newRow = [];
+            row.map((col, yIndex) => {
+                const newTile = {};
+                newTile.tileObj = this.randomTile();
+                newTile.xIndex = xIndex;
+                newTile.yIndex = yIndex;
+                newRow.push(newTile);
+            });
+            newState.push(newRow);
+        });
+        this.setState({tile: newState, gameStarted: true});
+    }
+
+    renderBoard = () => {
+        return (this.state.tile.map(row => {
+            return(row.map(tile => {
+                const {tileObj, xIndex, yIndex} = tile;
+                return <Tile shape={tileObj.shape} color={tileObj.color} xIndex={xIndex} yIndex={yIndex} click={this.handleClicks}/>
+            }));
+        }));
+    }
+
+    handleClicks = (coordinates) => {
+        const firstClick = this.state.firstClick;
+        if(Object.keys(firstClick).length === 0){
+            this.setState({firstClick: coordinates});
+        }
+        else{
+            this.setState({secondClick: coordinates}, () => {
+                this.checkSecondClick()
+            });
+        }
+    }
+
+    checkSecondClick = () => {
+        const firstClick = this.state.firstClick;
+        const secondClick = this.state.secondClick;
+        if(((firstClick.x === secondClick.x) && (Math.abs(firstClick.y - secondClick.y) === 1)) || ((firstClick.y === secondClick.y) && (Math.abs(firstClick.x - secondClick.x) === 1))){
+            this.switchTiles();
+        }
+        else{
+            console.log("cannot swap tiles!");
+            this.setState({firstClick: {}, secondClick: {}});
+        }
+    }
+
+    switchTiles = () => {
+        const tiles = this.state.tile;
+        const firstClick = this.state.firstClick;
+        const secondClick = this.state.secondClick;
+        const firstTile = tiles[firstClick.x][firstClick.y].tileObj;
+        const secondTile = tiles[secondClick.x][secondClick.y].tileObj;
+        tiles[firstClick.x][firstClick.y].tileObj = secondTile;
+        tiles[secondClick.x][secondClick.y].tileObj = firstTile;
+        this.setState({tile: tiles, firstClick: {}, secondClick: {}});
     }
 
     render() {
         return (
             <div style={containerStyle} className="container">
                 <div className="row border border-dark">
-                    {this.state.tile.map((row, xIndex) => {
-                        return row.map((col, yIndex) => {
-                            const color = this.randomColor();
-                            return (
-                                <Tile color={color} xkey={`${xIndex}`} ykey={`${yIndex}`}>
-                                    <p>{color}</p>
-                                    <p>x:{xIndex} y:{yIndex}</p>
-                                </Tile>
-                            );
-                        })
-                    })}
+                    {this.state.gameStarted ? this.renderBoard() : this.startBoard() }    
                 </div>
             </div>
         )
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
 
 function Tile(props) {
     return (
-      <div style={{backgroundColor: props.color}} className="col-2 border border-dark text-center p-4">
-        { props.children }
+      <div 
+      style={{backgroundColor: props.color}}
+      className="col-2 border border-dark text-center p-4 tile"
+      onClick={() => props.click({x: props.xIndex, y: props.yIndex})}
+      >
+        { props.shape }
       </div>
     )
 
